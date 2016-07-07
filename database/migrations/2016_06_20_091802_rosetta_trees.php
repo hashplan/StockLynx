7ac28425b2ba346338902bd9ec30889e7ed6d620
mobileapi <roman@aumix.com>
2016-07-07 18:41:28 +0300

    fix migration

--- database/migrations/2016_06_20_091802_rosetta_trees.php
+++ database/migrations/2016_06_20_091802_rosetta_trees.php
@@ -15,7 +15,7 @@ class RosettaTrees extends Migration
         Schema::create('rosetta_trees', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->length(10)->unsigned();
-            $table->bigInteger('parent_id')->length(20)->unsigned();
+            $table->bigInteger('parent_id')->length(20)->unsigned()->nullable();
             $table->string('name');
             $table->string('comment');
             $table->enum('status', array('ACTIVE','DELETED'))->index();
